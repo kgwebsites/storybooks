9d1089f3d0c6217e5e49eb3b4ba5ede0ef110b5b
Kyle Goss <kyle@kgwebsites.com>
2017-09-30 23:06:13 -0700

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://kgwebsites:livestrong@ds147544.mlab.com:47544/storybooks-dev',
+    googleClientID: '31214146744-gnop9ur0ce2lp5ostjmaji46ipp5tfbg.apps.googleusercontent.com',
+    googleClientSecret: 'eT4Od912M2XClBDLM3N1KqSF'
+}
