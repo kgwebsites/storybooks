7963da02033987277909bbfde4b04936c787ad35
Kyle Goss <kyle@kgwebsites.com>
2017-10-01 02:34:28 -0700

    Started UI

--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,23 @@ router.get('/google', passport.authenticate('google', {
 
 router.get('/google/callback', passport.authenticate('google', {
     failureRedirect: '/' }
-), (req, res) => {
-    //Successful Auth, Redirect Home
-    res.redirect('/dashboard');
-
-}
-
+    ), (req, res) => {
+        //Successful Auth, Redirect Home
+        res.redirect('/dashboard');
+    }
 )
+
+router.get('/verify', (req, res) => {
+    if(req.user){
+        console.log(req.user);
+    } else {
+        console.log('Not Auth');
+    }
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
